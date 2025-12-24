46d1aea624ba6fe4ac56e4151d08a50833ccf39f
Ahmad Hmedan <ahmadhmedann@gmail.com>
2025-12-24 13:43:48 +0000

    problem02 has been solved

--- Problem_01.cpp
+++ Problem_01.cpp
@@ -1,6 +1,8 @@
 // write a program to fill a 3*3 matrix with random numbers
 
 #include <iostream>
+#include <string>
+#include <iomanip>
 using namespace std;
 int randomNumberBetween(short from, short to)
 {
@@ -8,12 +10,12 @@ int randomNumberBetween(short from, short to)
         std::swap(from, to);
     return (rand() % (to - from + 1)) + from;
 }
-void FillMatrixWithRandomNumbers(int arr[3][3])
+void FillMatrixWithRandomNumbers(int arr[3][3],short rows, short cols)
 {
     
-    for (int i = 0; i <3; i++)
+    for (int i = 0; i <rows; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j <cols; j++)
         {
             arr[i][j] = randomNumberBetween(1, 99);
         }
@@ -26,7 +28,7 @@ void printMatrix(int arr[3][3])
     {
         for (int j = 0; j < 3; j++)
         {
-            cout<<arr[i][j]<<"  ";
+            cout<<setw(3)<<arr[i][j]<<"  ";
         }
         cout<<endl;
     }
@@ -36,7 +38,7 @@ int main()
 {
     srand(time(nullptr));
     int arr[3][3];
-    FillMatrixWithRandomNumbers(arr);
+    FillMatrixWithRandomNumbers(arr,3,3);
     printMatrix(arr);
     
     return 0;
